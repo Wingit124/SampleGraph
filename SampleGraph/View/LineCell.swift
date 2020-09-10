c1a759f86d1c68fc07d8c51f68719ca45b794dc7
Wingit124 <19cm0119@jec.ac.jp>
2020-09-10 16:51:48 +0900

    サンプルで棒グラフを作ってみた、房の長さをtransformでやっているせいで中央に揃っちゃう。。。

---
+++ SampleGraph/View/LineCell.swift
@@ -0,0 +1,25 @@
+//
+//  LineCell.swift
+//  SampleGraph
+//
+//  Created by 高橋翼 on 2020/09/10.
+//  Copyright © 2020 高橋翼. All rights reserved.
+//
+
+import UIKit
+
+class LineCell: UICollectionViewCell {
+
+    @IBOutlet weak var strokeView: UIView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    func setData(ratio: Float) {
+        strokeView.transform = .identity
+        strokeView.transform = strokeView.transform.scaledBy(x: 1, y: CGFloat(ratio))
+    }
+
+}
---
+++ SampleGraph/ViewController/ViewController.swift
@@ -0,0 +1,70 @@
+//
+//  ViewController.swift
+//  SampleGraph
+//
+//  Created by 高橋翼 on 2020/09/10.
+//  Copyright © 2020 高橋翼. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var graphCollectionView: UICollectionView! {
+        didSet {
+            graphCollectionView.dataSource = self
+            graphCollectionView.register(UINib(nibName: "LineCell", bundle: nil), forCellWithReuseIdentifier: "cell")
+            graphCollectionView.collectionViewLayout = createLayout()
+        }
+    }
+    
+    private let dammy: [Int] = {
+        var d: [Int] = []
+        for _ in 0...500 {
+            d.append(Int.random(in: 0...100))
+        }
+        return d
+    }()
+    
+    private var max = -1
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        max = dammy.max() ?? -1
+    }
+    
+    func createLayout() -> UICollectionViewLayout {
+        //最小単位のアイテムのサイズ
+        let leadingItem = NSCollectionLayoutItem(
+            layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                               heightDimension: .fractionalHeight(1.0)))
+        leadingItem.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 5, bottom: 0, trailing: 5)
+        //グループ内でのアイテムのレイアウト
+        let group = NSCollectionLayoutGroup.horizontal(
+            layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                               heightDimension: .fractionalHeight(1.0)),
+            subitem: leadingItem, count: 15)
+        //セクションにグループのレイアウトを適応
+        let section = NSCollectionLayoutSection(group: group)
+        section.orthogonalScrollingBehavior = .groupPaging
+        //レイアウトを返す
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        return layout
+    }
+        
+}
+
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return dammy.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! LineCell
+        let ratio = Float(dammy[indexPath.row]) / Float(max)
+        cell.setData(ratio: ratio)
+        return cell
+    }
+}
+
