47e4f3547e03cebe4462c0ca2a9416d5005bb526
Wingit124 <19cm0119@jec.ac.jp>
2020-09-10 13:22:54 +0900

    Initial Commit

---
+++ SampleGraphTests/SampleGraphTests.swift
@@ -0,0 +1,34 @@
+//
+//  SampleGraphTests.swift
+//  SampleGraphTests
+//
+//  Created by 高橋翼 on 2020/09/10.
+//  Copyright © 2020 高橋翼. All rights reserved.
+//
+
+import XCTest
+@testable import SampleGraph
+
+class SampleGraphTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
